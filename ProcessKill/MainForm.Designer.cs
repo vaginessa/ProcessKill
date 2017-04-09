320157d4cda5e4ca99c082db32e5fffed21ce2a3
sam <sam>
2017-04-09 13:43:46 +0800

    Process Kill

---
+++ ProcessKill/MainForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace ProcessKill
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_Search = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.button_Refrsh = new System.Windows.Forms.Button();
+            this.button_Kill = new System.Windows.Forms.Button();
+            this.listBox_Processes = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listBox_Processes, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(292, 273);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 57F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.textBox_Search, 1, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(286, 27);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(6, 8);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 8, 3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Search:";
+            // 
+            // textBox_Search
+            // 
+            this.textBox_Search.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox_Search.Location = new System.Drawing.Point(60, 3);
+            this.textBox_Search.Name = "textBox_Search";
+            this.textBox_Search.Size = new System.Drawing.Size(223, 21);
+            this.textBox_Search.TabIndex = 1;
+            this.textBox_Search.TextChanged += new System.EventHandler(this.textBox_Search_TextChanged);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.button_Refrsh, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.button_Kill, 1, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 241);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(286, 29);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // button_Refrsh
+            // 
+            this.button_Refrsh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_Refrsh.Location = new System.Drawing.Point(0, 0);
+            this.button_Refrsh.Margin = new System.Windows.Forms.Padding(0);
+            this.button_Refrsh.Name = "button_Refrsh";
+            this.button_Refrsh.Size = new System.Drawing.Size(143, 29);
+            this.button_Refrsh.TabIndex = 0;
+            this.button_Refrsh.Text = "Refresh";
+            this.button_Refrsh.UseVisualStyleBackColor = true;
+            this.button_Refrsh.Click += new System.EventHandler(this.button_Refrsh_Click);
+            // 
+            // button_Kill
+            // 
+            this.button_Kill.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button_Kill.Location = new System.Drawing.Point(143, 0);
+            this.button_Kill.Margin = new System.Windows.Forms.Padding(0);
+            this.button_Kill.Name = "button_Kill";
+            this.button_Kill.Size = new System.Drawing.Size(143, 29);
+            this.button_Kill.TabIndex = 1;
+            this.button_Kill.Text = "Kill";
+            this.button_Kill.UseVisualStyleBackColor = true;
+            this.button_Kill.Click += new System.EventHandler(this.button_Kill_Click);
+            // 
+            // listBox_Processes
+            // 
+            this.listBox_Processes.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox_Processes.FormattingEnabled = true;
+            this.listBox_Processes.ItemHeight = 12;
+            this.listBox_Processes.Location = new System.Drawing.Point(3, 36);
+            this.listBox_Processes.Name = "listBox_Processes";
+            this.listBox_Processes.Size = new System.Drawing.Size(286, 199);
+            this.listBox_Processes.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProcessKill";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.TextBox textBox_Search;
+        private System.Windows.Forms.Button button_Refrsh;
+        private System.Windows.Forms.Button button_Kill;
+        private System.Windows.Forms.ListBox listBox_Processes;
+
+    }
+}
+
---
+++ ProcessKill/MainForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace ProcessKill
+{
+    public partial class MainForm : Form
+    {
+        private Process[] processes = null;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FetchAllProcesses()
+        {
+            this.processes = Process.GetProcesses();
+        }
+
+        private void KillProcess()
+        {
+            if (listBox_Processes.Items.Count == 0)
+            {
+                return;
+            }
+            try
+            {
+                foreach (Process process in this.processes)
+                {
+                    if (process.ProcessName.Equals(listBox_Processes.SelectedItem.ToString()))
+                    {
+                        process.Kill();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void AddContentToListBox()
+        {
+            List<string> list = new List<string>();
+            foreach (Process process in this.processes)
+            {
+                list.Add(process.ProcessName);
+            }
+            list = list.Distinct().ToList();
+            list.Sort();
+            listBox_Processes.DataSource = list;
+        }
+
+        private void AutoFilter()
+        {
+            string str = textBox_Search.Text.ToUpper();
+            int length = textBox_Search.Text.Length;
+            if (length == 0)
+            {
+                this.AddContentToListBox();
+                return;
+            }
+            List<string> list = new List<string>();
+            foreach (Process process in this.processes)
+            {
+                if (process.ProcessName.Length > length && process.ProcessName.Substring(0, length).ToUpper().Equals(str))
+                {
+                    list.Add(process.ProcessName);
+                }
+            }
+            list = list.Distinct().ToList();
+            list.Sort();
+            listBox_Processes.DataSource = list;
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            this.FetchAllProcesses();
+            this.AddContentToListBox();
+        }
+
+        private void button_Refrsh_Click(object sender, System.EventArgs e)
+        {
+            this.FetchAllProcesses();
+            this.AddContentToListBox();
+            textBox_Search.Clear();
+        }
+
+        private void button_Kill_Click(object sender, System.EventArgs e)
+        {
+            this.KillProcess();
+            this.FetchAllProcesses();
+            this.AddContentToListBox();
+            textBox_Search.Clear();
+        }
+
+        private void textBox_Search_TextChanged(object sender, System.EventArgs e)
+        {
+            this.AutoFilter();
+        }
+    }
+}
